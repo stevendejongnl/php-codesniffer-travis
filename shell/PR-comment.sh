1b815e0e6df1c6c56bcc0748a38a4420811eaba3
Steven de Jong <steven@steven-dejong.nl>
2018-10-10 12:56:31 +0200

    added PR comment shell

---
+++ shell/PR-comment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ev
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
+   # hey that's a pull request
+   curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+    -d "{\"body\": \"Build success! 👍 \"}" \
+    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+
+    echo "PR success!"
+fi
