24c2ea4bc4a939c367b05bf1144444a6388ba751
Steven de Jong <steven@steven-dejong.nl>
2018-10-10 14:46:48 +0200

    bug in shell

--- shell/PR-comment.sh
+++ shell/PR-comment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
-if [[ "$TRAVIS_PULL_REQUEST" != "false" && $TRAVIS_BRANCH == 'develop' ]] ; then
-   # hey that's a pull request into develop
+if [ ${TRAVIS_PULL_REQUEST} != "false" ] && [ "$TRAVIS_BRANCH" = "develop" ]; then
+  # hey that's a pull request into develop
    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
     -d "{\"body\": \"Build success! 👍 Ask your PO to review your PR \"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
@@ -9,7 +9,7 @@ if [[ "$TRAVIS_PULL_REQUEST" != "false" && $TRAVIS_BRANCH == 'develop' ]] ; then
     echo "PR success!"
 fi
 
-if [[ "$TRAVIS_PULL_REQUEST" != "false" && $TRAVIS_BRANCH != 'master' && $TRAVIS_BRANCH != 'develop' ]] ; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ $TRAVIS_BRANCH != 'master' ] && [ $TRAVIS_BRANCH != 'develop' ]; then
    # hey that's a pull request not into develop
    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
     -d "{\"body\": \"Build success! 👍 Let your whole team review this PR \"}" \
