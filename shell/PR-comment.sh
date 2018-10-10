9b3c91e8dc71d6fca94715bf52c4e4fe45cf9a29
Steven de Jong <steven@steven-dejong.nl>
2018-10-10 14:50:03 +0200

    Small changes

--- shell/PR-comment.sh
+++ shell/PR-comment.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -ev
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ $TRAVIS_BRANCH = 'master' ]; then
+   # hey that's a pull request not into develop
+   curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+    -d "{\"body\": \"Build success! 👍 Let's merge into master! \"}" \
+    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+
+    echo "PR success!"
+fi
+
 if [ ${TRAVIS_PULL_REQUEST} != "false" ] && [ "$TRAVIS_BRANCH" = "develop" ]; then
   # hey that's a pull request into develop
    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
